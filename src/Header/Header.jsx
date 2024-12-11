b2a262aa3735ded1a0c51bb26aa0e56b6442387d
blufuf <ilyafatkincool@yandex.ru>
2024-12-11 20:13:18 +0300

    init

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -5,11 +5,11 @@ import cart from './shopping-cart.svg'
 export default function Header(){
     return(
         <header>
-            <div class = 'menu-burger'>
+            <div className = 'menu-burger'>
                 <img src={burger} alt="Menu"/>
             </div>
             <h3>FourTricks</h3>
-            <div class = 'shopping-cart'>
+            <div className = 'shopping-cart'>
                 <img src={cart} alt="Cart"/>
             </div>
         </header>
