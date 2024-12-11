1857da951a6131abf690c97c1c4a60ee9133e221
blufuf <ilyafatkincool@yandex.ru>
2024-12-11 18:36:22 +0300

    init

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,9 +1,10 @@
+
+
 export default function Header(){
     return(
         <header>
-            <div class = "block">
-                <h3>FourTricks</h3>
-            </div>
+
+            <h3>FourTricks</h3>
         </header>
     )
 }
